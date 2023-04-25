32eefd8b1f2c681a76a75de28a368a15b04e9441
oumoukhalil <zsarr2021@gmail.com>
2023-04-25 13:46:32 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include<main.h>
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+
+	for (char i = 'a'; i <= 'z'; i++)
+
+		_putchar(i);
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _putchar(char);
+#ifndef main_h
+	#define main_h
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,3 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
