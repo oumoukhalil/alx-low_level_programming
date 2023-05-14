97953f61d20f60d244b17d2cbe08c7e77142a3ce
oumoukhalil <zsarr2021@gmail.com>
2023-05-13 19:38:53 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main -prints its name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: Always Zero
+*/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
+
