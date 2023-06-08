924d0b0051e9134c250e0dcb2a23c58c7e64a96e
oumoukhalil <zsarr2021@gmail.com>
2023-06-08 12:05:36 -0700

    Tasks.0.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_unit - convert a binary number to a integer
+ * @b:sting that contains the binary number
+ *
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int de_valu = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0 ; b[i] ; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		de_valu = 2 * de_valu + (b[i] - '0');
+	}
+	return (de_valu);
+}
+
